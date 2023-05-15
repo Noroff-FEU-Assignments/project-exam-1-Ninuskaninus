5508613733ac3f078f5e38c5d72fe295b10503a0
Ninuskaninus <ninjaa181@gmail.com>
2023-05-15 21:21:54 +0200

    Added javascript and design on index-page

---
+++ js/navigation.js
@@ -0,0 +1,31 @@
+import { getPosts } from './postData.js';
+
+
+const navContainer = document.querySelector('#categories');
+const navCategoriesTitle = document.createElement("h2");
+navCategoriesTitle.textContent = "Categories";
+navCategoriesTitle.style.color = "var(--brightPink)";
+navContainer.appendChild(navCategoriesTitle); 
+
+getPosts()
+  .then(posts => {
+    const categories = {};
+    posts.forEach(post => {
+      post.category.split(', ').forEach(category => {
+        if (!categories[category]) {
+          categories[category] = [];
+        }
+        categories[category].push(post);
+      });
+    });
+    Object.keys(categories).forEach(category => {
+      const navCategories = document.createElement("a");
+      navCategories.href = `category.html?category=${category}`;
+      navCategories.textContent = category;
+      navCategories.style.color = "var(--brightPink)"
+      
+     
+      navContainer.appendChild(navCategories);
+    });
+  })
+  .catch(error => console.error(error));
---
+++ js/postData.js
@@ -0,0 +1,29 @@
+const url = 'https://wpsite.codecrush.no/wp-json/wp/v2/posts?_embed=true';
+
+const getPosts = () => {
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const posts = data.map(post => {
+        const categoryNames = post._embedded['wp:term'][0].map(category => category.name).join(', ');
+        const date = new Date(post.date).toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' });
+        const authorName = post._embedded.author[0].name;
+        return {
+          title: post.title.rendered,
+          content: post.content.rendered,
+          image: post._embedded['wp:featuredmedia'][0].source_url,
+          date: date,
+          author: authorName,
+          category: categoryNames,
+          link: post.link,
+          tags: post.tags,
+          slug: post.slug,
+        };
+      });
+      console.log(posts);
+      return posts;
+    })
+    .catch(error => console.error(error));
+};
+
+export { getPosts };
