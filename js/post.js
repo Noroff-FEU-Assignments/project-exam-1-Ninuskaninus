af5e0311a43a3bd06fa75f43bb3bcc870ea55277
Ninuskaninus <ninjaa181@gmail.com>
2023-05-18 18:30:19 +0200

    Added blogposts design

--- js/post.js
+++ js/post.js
@@ -17,27 +17,73 @@ menuButtonClose.addEventListener("click", () => {
    menubutton.style.transition = "all 0.5s ease-in-out";
 });
 
-const urlParams = new URLSearchParams(window.location.search);
-const postId = urlParams.get("id");
+getPosts().then(posts => {
+    const selectedPost = posts.find(post => post.id === parseInt(location.search.split("=")[1]));
+    const blogheader = document.querySelector(".postHead");
+    blogheader.style.backgroundImage = `url(${selectedPost.image})`;
+
+    const blogHeadContainer = document.createElement("div");
+    blogHeadContainer.classList.add("postHeadContent");
+    blogheader.appendChild(blogHeadContainer);
+
+    const author = document.createElement("p");
+    author.id = "blue";
+    author.textContent = `> Written by ${selectedPost.author}`;
+    blogHeadContainer.appendChild(author);
+
+    const date = document.createElement("p");
+    date.id = "yellow";
+    date.textContent = `> Posted: ${selectedPost.date}`;
+    blogHeadContainer.appendChild(date);
+
+    const likes = document.createElement("p");
+    likes.id = "pink";
+    likes.textContent = `> Likes: 1500`;
+    blogHeadContainer.appendChild(likes);
+
+    const blogPost = document.querySelector(".blogPost");
+    const blogTitle = document.createElement("h1");
+    blogTitle.textContent = selectedPost.title;
+    blogPost.appendChild(blogTitle);
+
+    const blogContent = document.createElement("div");
+    blogContent.classList.add("blogContent");
+
+const codeContent = selectedPost.content;
+const highlightedContent = codeContent.replace(/(const|let|var)/g, '<span class="keyword">$1</span>');
+
+    blogContent.innerHTML = selectedPost.content;
+    blogPost.appendChild(blogContent);
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Wait for the DOM to be fully loaded before executing the code
+  
+    const copyButton = document.querySelector(".wp-block-buttons");
+    const copyCode = document.querySelector("code");
+  
+    if (copyButton && copyCode) {
+      // Check if both elements are present in the DOM
+  
+      copyButton.style.color = "blue";
+  
+      copyButton.addEventListener("click", function() {
+        copyCode.select();
+        document.execCommand("copy");
+        alert("Code copied to clipboard!");
+      });
+    }
+  });
+  
 
 
 
-getPosts(postId)
-  .then(post => {
-    const browserTitle = document.querySelector("title");
-    browserTitle.textContent = post.title;
-
-    const postHead = document.querySelector(".postHead");
-    postHead.style.backgroundImage = `url(${post.image})`;
-
-    const postHeadContent = document.createElement("div");
-    postHeadContent.classList.add("postHeadContent");
-    postHead.appendChild(postHeadContent);
 
 
-   
-  })
-  .catch(error => console.error(error));
+
+
+
+
 
 
   
