0339dbbba4742e7f83a69b31d2b58e01552f97d5
Ninuskaninus <ninjaa181@gmail.com>
2023-05-21 14:37:01 +0200

    Added categories post

--- js/categories.js
+++ js/categories.js
@@ -1,31 +1,37 @@
-// Import post data from getPosts       .js
-import { getPosts        } from './getPosts       .js';
+import { getPosts } from './postData.js';
 
-// Function to display the posts based on the selected category
-function displayCategoryPosts(categoryId) {
-    const categoryPostContainer = document.getElementById('categoryPostContainer');
-    categoryPostContainer.innerHTML = ''; // Clear the existing content
+getPosts().then(posts => {
+    const postsSection = document.querySelector(".categoryPosts");
+    const postContainer = document.createElement("div");
+    postContainer.classList.add("categoryPostContainer");
+    postsSection.appendChild(postContainer);
 
-    // Filter the post data based on the selected category
-    const posts = getPosts       .filter(post => post.category === categoryId);
+    const urlParams = new URLSearchParams(window.location.search);
+    const category = urlParams.get("category");
 
-    // Iterate over the filtered posts and create HTML elements dynamically
-    posts.forEach(post => {
-        const categoryPostItem = document.createElement('div');
-        categoryPostItem.className = 'categoryPostItem';
+    const categoryHeader = document.querySelector(".categoriesHead");
+    const categoryHeaderTitle = document.createElement("h1");
+    categoryHeaderTitle.textContent = category;
+    categoryHeader.appendChild(categoryHeaderTitle);
 
-        const image = document.createElement('img');
-        image.src = post.image;
-        image.alt = '';
+    for (let i = 0; i < posts.length; i++) {
+        const post = posts[i];
 
-        const title = document.createElement('h6');
-        title.textContent = post.title;
+        const postItem = document.createElement("a");
+        postItem.href = `post.html?id=${post.id}`;
+        postItem.classList.add("categoryPostItem");
+        postContainer.appendChild(postItem);
 
-        categoryPostItem.appendChild(image);
-        categoryPostItem.appendChild(title);
-        categoryPostContainer.appendChild(categoryPostItem);
-    });
-}
+        const postSpan = document.createElement("span");
+        postSpan.classList.add("line");
+        postContainer.appendChild(postSpan);
 
-// Example usage: You can call this function when a category is clicked
-displayCategoryPosts('category1');
+        const postImg = document.createElement("img");
+        postImg.src = post.image;
+        postItem.appendChild(postImg);
+
+        const postTitle = document.createElement("h6");
+        postTitle.textContent = post.title;
+        postItem.appendChild(postTitle);
+    }
+});
